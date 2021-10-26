84fbb4f913359791085bdf8b0b61984d32b0e877
UrkoU <ikbqa@plaiaundi.net>
2021-10-26 12:38:53 +0200

    Paso 4

--- PruebaInicialASPNetCore/Startup.cs
+++ PruebaInicialASPNetCore/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PruebaInicialASPNetCore.Data;
 
 namespace PruebaInicialASPNetCore
 {
@@ -24,6 +26,9 @@ namespace PruebaInicialASPNetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<PruebaInicialASPNetCoreContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("PruebaInicialASPNetCoreContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
